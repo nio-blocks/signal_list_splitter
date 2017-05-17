983610e27e19d5f203265b163a1808467fe6f9a4
Tom Young <tyoung@n.io>
2017-05-17 15:07:58 -0600

    Add comments

--- tests/test_signal_list_splitter_block.py
+++ tests/test_signal_list_splitter_block.py
@@ -7,17 +7,20 @@ from ..signal_list_splitter_block import SignalListSplitter
 class TestTheThings(NIOBlockTestCase):
 
     def signals_notified(self, block, signals, output_id):
+        # override to make list of lists of signals notified
         self.last_notified[output_id].append(signals)
 
     def test_process_signals(self):
-        """Signals pass through block unmodified."""
+        """List of multiple signals is split into lists of single signals"""
         blk = SignalListSplitter()
         self.configure_block(blk, {})
         blk.start()
+        # one list of 3 signals is processed
         blk.process_signals([Signal({"hello": "n.io"}),
                              Signal({"hello": "n.io"}),
                              Signal({"hello": "n.io"})])
         blk.stop()
         self.assert_num_signals_notified(3)
+        # and three lists of one signal have been notified
         self.assertEqual(
             len(self.last_notified[DEFAULT_TERMINAL]), 3)
