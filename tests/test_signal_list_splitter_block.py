39a858050d96de861840011f3e3586f25465c867
Tom Young <tyoung@n.io>
2017-05-18 09:53:32 -0600

    finalize unit test and comments

--- tests/test_signal_list_splitter_block.py
+++ tests/test_signal_list_splitter_block.py
@@ -4,23 +4,31 @@ from nio.testing.block_test_case import NIOBlockTestCase
 from ..signal_list_splitter_block import SignalListSplitter
 
 
-class TestTheThings(NIOBlockTestCase):
+class TestProcessSignalList(NIOBlockTestCase):
+
+    input_signals = [Signal({"hello": "n.io"}),
+                     Signal({"hello": "n.io"}),
+                     Signal({"hello": "n.io"})]
 
     def signals_notified(self, block, signals, output_id):
-        # override to make list of lists of signals notified
+        """override function so that last_notified is list of lists of signals
+        notified
+        """
         self.last_notified[output_id].append(signals)
 
-    def test_process_signals(self):
-        """List of multiple signals is split into lists of single signals"""
+    def test_process_signal_list(self):
+        """List of signals is split into lists of single signals"""
         blk = SignalListSplitter()
         self.configure_block(blk, {})
         blk.start()
-        # one list of 3 signals is processed
-        blk.process_signals([Signal({"hello": "n.io"}),
-                             Signal({"hello": "n.io"}),
-                             Signal({"hello": "n.io"})])
+        # one list of signals is processed
+        blk.process_signals(self.input_signals)
         blk.stop()
-        self.assert_num_signals_notified(3)
-        # and three lists of one signal have been notified
-        self.assertEqual(
-            len(self.last_notified[DEFAULT_TERMINAL]), 3)
+        self.assert_num_signals_notified(len(self.input_signals))
+        # and one list of one signal has been notified for each signal
+        self.assertEqual(len(self.last_notified[DEFAULT_TERMINAL]),
+                         len(self.input_signals))
+
+class TestProcessSignalListOfOne(TestProcessSignalList):
+
+    input_signals = [Signal({"hello": "n.io"})]
