f98a6576d1b1c77159e72e4a57e632b1b2b75664
neutralioJenkins <jenkins@n.io>
2018-01-08 15:26:35 -0700

    Bump version to v0.1.1

--- signal_list_splitter_block.py
+++ signal_list_splitter_block.py
@@ -4,7 +4,7 @@ from nio.properties import VersionProperty
 
 class SignalListSplitter(Block):
 
-    version = VersionProperty("0.1.0")
+    version = VersionProperty("0.1.1")
 
     def process_signals(self, signals):
         for signal in signals:
